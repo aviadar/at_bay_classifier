e315674b1f4b37457f810db903a3be6d08822bad
aviadar <aviad222@gmail.com>
2022-09-30 12:28:31 -0700

    updates

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
 
     logging.error('processing text')
     for index, row in tqdm(df.iterrows()):
-        # if row.ouput_dir[27:] != 'year=2022/month=9/day=12/hour=15/minutes=25/ad587506-c925-4347-a3f4-a28f28770a00':
+        # if row.ouput_dir[27:] != 'year=2022/month=9/day=12/hour=15/minutes=26/b9e61c59-3195-4624-98c4-f5720bc32b14':
         #     continue
         if index < 95:
             continue
